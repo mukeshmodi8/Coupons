30e215769181756505b0c4333500fa8542ffeb9f
mukeshmodi8 <mvmodi.2014@gmail.com>
2025-09-14 18:59:39 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Claim from "./components/Claim";
+import Claim from "./components/claime";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
